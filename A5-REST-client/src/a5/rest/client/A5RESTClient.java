d04dda07cc222664e00bf1576f44fafb02b43a36
vemiz <tomas_paulsen@hotmail.com>
2018-10-23 12:51:13 +0200

    Initial Push. Implemented example code "HTTP GET request" as part of the tasks: Getting started 1-7

---
+++ A5-REST-client/src/a5/rest/client/A5RESTClient.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a5.rest.client;
+
+
+
+/**
+ *
+ * @author tomas
+ */
+public class A5RESTClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        runExample(2);
+    }
+ static void runExample(int num){
+    if(num==1){
+        get_example example = new get_example("104.248.47.74", 80);
+        example.doExampleGet(); //http://104.248.47.74/dkrest/test/get
+    }
+    if(num==2){
+        get_example example = new get_example("104.248.47.74", 80);
+        example.doExampleGet2(); //http://104.248.47.74/dkrest/test/get2
+    }
+ }   
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+Assignment A5: WebService client 
+Task: create an application that acts as a client for a REST web service.
+The server side (service) will be provided by the teacher. 
+It is recommended to read the whole document before starting the coding. 
+This time the grades will be decided by a robot, therefore it is important to stick with the protocol. 
+The robot will not appreciate your improvisation skills :)
